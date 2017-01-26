7d631bd6105ca29ee0dabc97887b8d09d8be7671
Jimena Cabrera Notari <phenomenes@gmail.com>
2017-01-26 19:48:09 +0000

    resurrecting project
    - make it work with the latest Ansible version
    - use env var to find ansible module's path
    - use yaml syntax
    - refactor code

--- snippet_generator.py
+++ snippet_generator.py
@@ -4,50 +4,46 @@ from ansible.utils.module_docs import get_docstring
 import os
 import sys
 
-# add play by default
-print "snippet play"
-print "	- hosts: ${1:group}"
-print "	  user: ${2:root}"
-print "	  tasks:"
-print 
+modpath = os.environ["ANSIBLE_MODULE_PATH"]
 
-libpath = os.path.abspath(os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), os.pardir, 'library'
-    ))
-
-fname_list = []
-
-for d in os.listdir(libpath):
-    for f in os.listdir(os.path.join(libpath, d)):
-        fname_list.append(os.path.join(libpath, d, f))
-
-for fname in fname_list: 
-    if os.path.isdir(fname):
-        continue
-
-    doc, examples = get_docstring(fname)
+def generate(path):
+    doc, examples, _ = get_docstring(path)
     if doc != None:
-        print "snippet %s" % (doc['module'])
-        print "	- name: ${1:task_description}"
-        print "	  %s:" % (doc['module']),
+        print "snippet %s" % doc['module']
+        print "- name: ${1:task_description}"
+        print "  %s: " % doc['module']
+
         if 'options' in doc:
             count = 1
-            for o in doc['options']:
-                if 'required' in doc['options'][o] and doc['options'][o]['required']:
-                    count += 1
-                    if 'default' in doc['options'][o] and doc['options'][o]['default']:
-                        value = "${%d:%s}" % (count, doc['options'][o]['default'])
+            for opt in doc['options']:
+                count += 1
+                if 'required' in doc['options'][opt] and doc['options'][opt]['required']:
+                    if 'default' in  doc['options'][opt]:
+                        value = "${%d:%s}" % (count, doc['options'][opt]['default'])
                     else:
                         value = "${%d}" % (count)
-                    print "%s=%s" % (o, value),
-            print "#",
-            for o in doc['options']:
-                if not 'required' in doc['options'][o] or doc['options'][o]['required'] == False:
+                    print "    %s: %s" % (opt, value)
+
+            for opt in doc['options']:
+                if not 'required' in doc['options'][opt] or doc['options'][opt]['required'] == False:
                     count += 1
-                    if 'default' in doc['options'][o] and doc['options'][o]['default']:
-                        value = "${%d:%s}" % (count, doc['options'][o]['default'])
+                    if 'default' in doc['options'][opt] and doc['options'][opt]['default']:
+                        value = "${%d:%s}" % (count, doc['options'][opt]['default'])
                     else:
                         value = "${%d}" % (count)
-                    print "%s=%s" % (o, value),
-            print
-    print
+                    print "    #%s: %s" % (opt, value)
+                print 'endsnippet'
+                print
+
+print '''
+snippet play
+- hosts: ${1:group}
+  user: ${2:root}
+  tasks:
+endsnippet
+'''
+
+for root, _, files in os.walk(modpath):
+    for name in files:
+        if name.endswith('.py'):
+            generate(os.path.join(root, name))
