c48b2fcbba7535aee863644b1d813824a7376658
Jimena Cabrera Notari <phenomenes@gmail.com>
2017-01-27 14:36:32 +0000

    add snipmate support

--- README.md
+++ README.md
@@ -1,42 +1,18 @@
-ansible_snippet_generator
-=========================
+# ansible_snippet_generator
 
-A small script to generate snipmate.vim snippets to be used in ansible playbooks
+A small script to generate snipmate.vim and UltiSnips Ansible snippets.
 
-Requierements
--------------
+## Requierements
 
-- ansible sources: https://github.com/ansible/ansible / http://ansible.cc
-- snipmate.vim: https://github.com/msanders/snipmate.vim
+- [Ansible source](/ansible/ansible)
+- [snipmate.vim](/msanders/snipmate.vim) or [ultisnips](/SirVer/ultisnips)
 
-What does it do ?
------------------
+## Run
 
-This scripts will parse the DOCUMENTATION strings of ansible module, and
-generate snippets to be used with snipmate.vim. The snippets will use the module
-name, and provides all the requireds parameters directly, and the optional
-parameters in comment.
+```
+./ansible_snippet_generator.py --ultisnips ~/ansible/lib/ansible/modules > ~/.vim/snippets/UltiSnips/yaml.snippets
+```
 
-Output is done on STDOUT, you can redirect it to your
-snipmate.vim/snippets/yaml.snippets if you wish.
+## Credits
 
-Usage
------
-
-Installation:
-
-- put the script in ansible/hacking (so it can find module_formatter.py)
-- execute and redirect output to a file
-
-In vim:
-- Edit a .yaml file
-- type `play` and then _\<tab\>_, it will fill the start of the playbook
-- the cursor will be placed on the next token, for playbook this is the hosts
-  variable, you can then jump between tokens with _\<tab\>_ and _\<S-tab\>_
-- then you can add an action for a given module by typing the name of the module
-  followed by _\<tab\>_
-
-Example
--------
-
-Have a look at this asciiio cast if you wish: http://ascii.io/a/1524
+Forked from [bleader/ansible_snippet_generator](/bleader/ansible_snippet_generator)
--- snippet_generator.py
+++ snippet_generator.py
@@ -1,41 +1,11 @@
 #!/usr/bin/env python2
 
+import argparse
 from ansible.utils.module_docs import get_docstring
 import os
 import sys
 
-modpath = os.environ["ANSIBLE_MODULE_PATH"]
-
-def generate(path):
-    doc, examples, _ = get_docstring(path)
-    if doc != None:
-        print "snippet %s" % doc['module']
-        print "- name: ${1:task_description}"
-        print "  %s: " % doc['module']
-
-        if 'options' in doc:
-            count = 1
-            for opt in doc['options']:
-                count += 1
-                if 'required' in doc['options'][opt] and doc['options'][opt]['required']:
-                    if 'default' in  doc['options'][opt]:
-                        value = "${%d:%s}" % (count, doc['options'][opt]['default'])
-                    else:
-                        value = "${%d}" % (count)
-                    print "    %s: %s" % (opt, value)
-
-            for opt in doc['options']:
-                if not 'required' in doc['options'][opt] or doc['options'][opt]['required'] == False:
-                    count += 1
-                    if 'default' in doc['options'][opt] and doc['options'][opt]['default']:
-                        value = "${%d:%s}" % (count, doc['options'][opt]['default'])
-                    else:
-                        value = "${%d}" % (count)
-                    print "    #%s: %s" % (opt, value)
-                print 'endsnippet'
-                print
-
-print '''
+ultisnips_play = '''
 snippet play
 - hosts: ${1:group}
   user: ${2:root}
@@ -43,7 +13,50 @@ snippet play
 endsnippet
 '''
 
-for root, _, files in os.walk(modpath):
-    for name in files:
-        if name.endswith('.py'):
-            generate(os.path.join(root, name))
+snipmate_play = '''
+snippet play
+- hosts: ${1:group}
+  user: ${2:root}
+  tasks:
+'''
+
+def generate(path, args):
+    doc, examples, _ = get_docstring(path)
+    prefix = args.snipmate and '\t' or ''
+    if doc != None:
+        print "snippet %s" % doc['module']
+        print "%s- name: ${1:task_description}" % prefix
+        print "%s  %s: " % (prefix, doc['module'])
+
+        count = 1
+        if 'options' in doc:
+            for opt in doc['options']:
+                count += 1
+                optional = not doc['options'][opt].get('required', False)
+                if 'default' in  doc['options'][opt]:
+                    value = "${%d:%s}" % (count, doc['options'][opt]['default'])
+                else:
+                    value = "${%d}" % (count)
+                print "%s    %s%s: %s" % (prefix, optional and '#' or '', opt, value)
+            if args.ultisnips:
+                print 'endsnippet'
+            print
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='snippet_generator.py')
+    parser.add_argument('--ultisnips', action='store_true',
+    	help='Generate snippets for UltiSnips')
+    parser.add_argument('--snipmate', action='store_true',
+    	help='Generate snippets for snipMate')
+    parser.add_argument('modpath', type=str, nargs=1,
+        help="Path to Ansible's modules, i.e.:/<ansible_source>/lib/ansible/modules")
+    args = parser.parse_args()
+
+    for root, _, files in os.walk(args.modpath[0]):
+        for name in files:
+            if name.endswith('.py'):
+                generate(os.path.join(root, name), args)
+
+if __name__ == '__main__':
+        main()
